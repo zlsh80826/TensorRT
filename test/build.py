c8c62f7adefb04e741256392c91b752410a1d586
zlsh80826 <zlsh80826@gmail.com>
2020-10-28 13:52:48 +0800

    var-len

--- test/build.py
+++ test/build.py
@@ -12,8 +12,8 @@ def get_trt_plugin(plugin_name, input_tensor, dtype):
         plugin = None
         for plugin_creator in PLUGIN_CREATORS:
             print(plugin_creator.name)
-            if plugin_creator.name == plugin_name:
-                B, S, hidden_size, _, _ = input_tensor.shape
+            if plugin_creator.name == plugin_name and plugin_creator.plugin_version == "2":
+                _, hidden_size, _, _ = input_tensor.shape
                 hidden_size = int(hidden_size / 3)
                 num_heads = 12
                 head_size = int(hidden_size / num_heads)
@@ -21,8 +21,9 @@ def get_trt_plugin(plugin_name, input_tensor, dtype):
                 pf_hidden_size = trt.PluginField("hidden_size", np.array([hidden_size], np.int32), trt.PluginFieldType.INT32)
                 pf_num_heads = trt.PluginField("num_heads", np.array([num_heads], np.int32), trt.PluginFieldType.INT32)
                 pf_has_mask = trt.PluginField("has_mask", np.array([1], np.int32), trt.PluginFieldType.INT32)
+                pf_var_seqlen = trt.PluginField("var_seqlen", np.array([1], np.int32), trt.PluginFieldType.INT32)
 
-                field_collection = trt.PluginFieldCollection([pf_hidden_size, pf_num_heads, pf_has_mask, pf_type])
+                field_collection = trt.PluginFieldCollection([pf_hidden_size, pf_num_heads, pf_has_mask, pf_type, pf_var_seqlen])
                 plugin = plugin_creator.create_plugin(name=plugin_name, field_collection=field_collection)
         return plugin
 
@@ -41,13 +42,26 @@ def main(args):
         if args.fp16:
             dtype = trt.float16
 
-        input_shape = (128, 1, 768 * 3, 1, 1)
-        mask_shape = (input_shape[1], int(input_shape[2]/3))
-        input_tensor = network.add_input(name="input_layer", dtype=dtype, shape=input_shape)
-        input_mask_tensor = network.add_input(name="input_mask_layer", dtype=dtype, shape=mask_shape)
-        qkv = network.add_plugin_v2(inputs=[input_tensor, input_mask_tensor], 
+
+        # assumn the input is 4 sequence, lens are 31,33,93,67
+        # input len = [31,33,93,67]
+        # mask = [num_seq, fp16maskSize] 
+        # cu_seq_len = [num_seq + 1]
+        # dummy_tensor = [max_seq_len]
+
+        input_shape = (224, 768 * 3, 1, 1)
+        mask_shape = (4, 1024)
+        cu_shape = (5,)
+        dummy_shape = (93,)
+
+        i0 = network.add_input(name='i0', dtype=dtype, shape=input_shape)
+        i1 = network.add_input(name='i1', dtype=dtype, shape=mask_shape)
+        i2 = network.add_input(name='i2', dtype=trt.int32, shape=cu_shape)
+        i3 = network.add_input(name='i3', dtype=trt.float32, shape=dummy_shape)
+
+        qkv = network.add_plugin_v2(inputs=[i0, i1, i2, i3], 
                                     plugin=get_trt_plugin("CustomQKVToContextPluginDynamic", 
-                                                          input_tensor, dtype))
+                                                          i0, dtype))
         qkv.get_output(0).name = "output"
         network.mark_output(qkv.get_output(0))
         engine = builder.build_engine(network, builder_config)
--- test/inference.py
+++ test/inference.py
@@ -31,6 +31,8 @@ if __name__ == '__main__':
         assert context.all_binding_shapes_specified
         input_shape = context.get_binding_shape(0)
         mask_shape = context.get_binding_shape(1)
+        cu_seq_len_shape = context.get_binding_shape(2)
+        dummy_shape = context.get_binding_shape(3)
 
         # Create a stream in which to copy inputs/outputs and run inference.
         stream = cuda.Stream()
@@ -38,22 +40,30 @@ if __name__ == '__main__':
         # Allocate device memory for inputs.
         input_nbytes = trt.volume(input_shape) * dtype.itemsize
         input_mask_nbytes = trt.volume(mask_shape) * dtype.itemsize
-        d_inputs = [cuda.mem_alloc(input_nbytes), cuda.mem_alloc(input_mask_nbytes)]
-        print(int(d_inputs[0]))
-        print(int(d_inputs[1]))
+        cu_seq_len_nbytes = trt.volume(cu_seq_len_shape) * dtype.itemsize
+        dummy_len_nbytes = trt.volume(dummy_shape) * dtype.itemsize
+        d_inputs = [cuda.mem_alloc(input_nbytes), cuda.mem_alloc(input_mask_nbytes),
+                    cuda.mem_alloc(cu_seq_len_nbytes), cuda.mem_alloc(dummy_len_nbytes)]
 
         np.random.seed(0)
-        sample_input = ((np.random.rand(input_shape[0], input_shape[1], input_shape[2], input_shape[3], input_shape[4]) - 0.5) * 1e-3).astype(nptype)
-        sample_mask_input = np.ones((mask_shape[0],int(mask_shape[1]/2))).astype(np.int32)
+        sample_input = ((np.random.rand(input_shape[0], input_shape[1], input_shape[2], input_shape[3]) - 0.5) * 1e-3).astype(nptype)
+        sample_mask_input = np.ones((mask_shape[0], int(mask_shape[1]))).astype(nptype)
+        sample_cu_seq_len = np.array([0, 31, 31 + 33, 31 + 33 + 93, 31 + 33 + 93 + 67], dtype=np.int32)
+        sample_dummy = np.zeros((93,), dtype=np.float32)
 
         # Allocate output buffer by querying the size from the context. This may be different for different input shapes.
-        h_output = cuda.pagelocked_empty(tuple(context.get_binding_shape(2)), dtype=nptype)
+        h_output = cuda.pagelocked_empty(tuple(context.get_binding_shape(4)), dtype=nptype)
         d_output = cuda.mem_alloc(h_output.nbytes)
 
         h_sample_input = cuda.register_host_memory(np.ascontiguousarray(sample_input.ravel()))
         h_sample_mask_input = cuda.register_host_memory(np.ascontiguousarray(sample_mask_input.ravel()))
+        h_sample_cu_seq_len = cuda.register_host_memory(np.ascontiguousarray(sample_cu_seq_len.ravel()))
+        h_sample_dummy = cuda.register_host_memory(np.ascontiguousarray(sample_dummy.ravel()))
+
         cuda.memcpy_htod_async(d_inputs[0], h_sample_input, stream)
         cuda.memcpy_htod_async(d_inputs[1], h_sample_mask_input, stream)
+        cuda.memcpy_htod_async(d_inputs[2], h_sample_cu_seq_len, stream)
+        cuda.memcpy_htod_async(d_inputs[3], h_sample_dummy, stream)
 
         context.execute_async_v2(bindings=[int(i) for i in d_inputs] + [int(d_output)], stream_handle=stream.handle)
         stream.synchronize()
@@ -61,4 +71,4 @@ if __name__ == '__main__':
         stream.synchronize()
 
         print(h_sample_input[: 10])
-        print(h_output[:, 0, 0, 0, 0])
+        print(h_output[:, 0, 0, 0])
